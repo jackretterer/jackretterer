a40789cb6e299331c6553cd76abb0c041b63818e
jackretterer <jack.retterer@gmail.com>
2023-12-14 09:51:00 -0800

    Fixed some dependency issues

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,47 +1,39 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Button, IconButton } from '@mui/material';
-import { makeStyles } from '@mui/styles';
+import { styled } from '@mui/material/styles';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import logo from '../assets/logo.png';
 
-const useStyles = makeStyles((theme) => ({
-  logo: {
-    flexGrow: 1,
-    color: 'black',
-    textDecoration: 'none',
-  },
-  navLinks: {
-    '& .MuiButton-root': {
-      color: 'black',
-      marginRight: theme.spacing(2),
-      textDecoration: 'none',
-      textTransform: 'none', // This prevents the text from being automatically capitalized
-    },
-  },
+const StyledTypography = styled(Typography)(({ theme }) => ({
+  flexGrow: 1,
+  color: 'black',
+  textDecoration: 'none',
 }));
 
-const NavBar = () => {
-  const classes = useStyles();
+const StyledIconButton = styled(IconButton)({
+  color: 'black',
+  '&:hover': { backgroundColor: 'transparent', opacity: 1 }
+});
 
+const NavBar = () => {
   return (
-    <AppBar position="sticky" sx={{ backgroundColor: '#EEEEEE' }} elevation={0}>
+    <AppBar position="sticky" sx={{ backgroundColor: '#EEEEEE', elevation: 0 }}>
       <Toolbar>
-        <Typography variant="h6" className={classes.logo} component={Link} to="/" style={{ textDecoration: 'none' }}>
-          <img src={logo} alt="Logo" style={{ height: '40px' }} /> {/* Adjust height as needed */}
-        </Typography>
-        <div className={classes.navLinks}>
-          <Button color="inherit" href="#about" sx={{'&:hover': { backgroundColor: 'transparent', opacity: 1}}}>About</Button>
-          <Button color="inherit" href="#experience" sx={{'&:hover': { backgroundColor: 'transparent', opacity: 1}}}>Experience</Button>
-          <Button color="inherit" href="#projects" sx={{'&:hover': { backgroundColor: 'transparent', opacity: 1}}}>Projects</Button>
-          <IconButton color="inherit" href="https://twitter.com/jaretterer" target="_blank" sx={{'&:hover': { backgroundColor: 'transparent', opacity: 1}}}>
-            <FontAwesomeIcon icon={faTwitter} style={{ color: 'black' }} />
-          </IconButton>
-          <IconButton color="inherit" href="https://linkedin.com/jackretterer" target="_blank" sx={{'&:hover': { backgroundColor: 'transparent', opacity: 1}}}>
-            <FontAwesomeIcon icon={faLinkedin} style={{ color: 'black' }}/>
-          </IconButton>
+        <StyledTypography variant="h6" component={Link} to="/" sx={{ textDecoration: 'none' }}>
+          <img src={logo} alt="Logo" style={{ height: '40px' }} />
+        </StyledTypography>
+        <div>
+          <Button color="inherit" href="#about" sx={{ color: 'black', marginRight: 2, textDecoration: 'none', textTransform: 'none', '&:hover': { backgroundColor: 'transparent', opacity: 1 }}}>About</Button>
+          <Button color="inherit" href="#experience" sx={{ color: 'black', marginRight: 2, textDecoration: 'none', textTransform: 'none', '&:hover': { backgroundColor: 'transparent', opacity: 1 }}}>Experience</Button>
+          <Button color="inherit" href="#projects" sx={{ color: 'black', textDecoration: 'none', textTransform: 'none', '&:hover': { backgroundColor: 'transparent', opacity: 1 }}}>Projects</Button>
+          <StyledIconButton href="https://twitter.com/jaretterer" target="_blank">
+            <FontAwesomeIcon icon={faTwitter} />
+          </StyledIconButton>
+          <StyledIconButton href="https://linkedin.com/jackretterer" target="_blank">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </StyledIconButton>
         </div>
       </Toolbar>
     </AppBar>
