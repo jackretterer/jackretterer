c9628a4de9913e68a327e71693c5da64672ebf84
jackretterer <jack.retterer@gmail.com>
2023-12-14 17:03:10 -0800

    Added hamburger menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Button, IconButton } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { AppBar, Toolbar, Typography, Button, IconButton, List, ListItemText, ListItem, Drawer } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import { styled } from '@mui/material/styles';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -18,16 +19,63 @@ const StyledIconButton = styled(IconButton)({
 });
 
 const NavBar = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <>
+      <List>
+        {['About', 'Experience', 'Projects'].map((text) => (
+          <ListItem button key={text} sx={{textTransform: 'none'}} component="a" href={`#${text.toLowerCase()}`}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <StyledIconButton href="https://twitter.com/jaretterer" target="_blank">
+        <FontAwesomeIcon icon={faTwitter} />
+      </StyledIconButton>
+      <StyledIconButton href="https://linkedin.com/jackretterer" target="_blank">
+        <FontAwesomeIcon icon={faLinkedin} />
+      </StyledIconButton>
+    </>
+  );
+
   return (
     <AppBar position="sticky" sx={{ backgroundColor: '#EEEEEE', elevation: 0, borderBottom: 'none' }}>
-      <Toolbar sx={{ padding: '0px', margin: '0px' }}>
+      <Toolbar sx={{ padding: '0 16px', margin: '0px' }}>
         <StyledTypography variant="h6" component={Link} to="/" sx={{ textDecoration: 'none' }}>
           <img src={logo} alt="Logo" style={{ height: '40px' }} />
         </StyledTypography>
-        <div>
-          <Button color="inherit" href="#about" sx={{ color: 'black', marginRight: 2, textDecoration: 'none', textTransform: 'none', '&:hover': { backgroundColor: 'transparent', opacity: 1 }}}>About</Button>
-          <Button color="inherit" href="#experience" sx={{ color: 'black', marginRight: 2, textDecoration: 'none', textTransform: 'none', '&:hover': { backgroundColor: 'transparent', opacity: 1 }}}>Experience</Button>
-          <Button color="inherit" href="#projects" sx={{ color: 'black', textDecoration: 'none', textTransform: 'none', '&:hover': { backgroundColor: 'transparent', opacity: 1 }}}>Projects</Button>
+        { isMobile ? 
+        <IconButton
+          aria-label="open drawer"
+          edge="end"
+          onClick={handleDrawerToggle}
+          sx={{ color: 'black' }}
+        >
+          <MenuIcon />
+        </IconButton>
+        : null }
+        { isMobile ? null :
+        <div sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center' }}>
+          <Button color="inherit" href="#about" sx={{ color: 'black', textDecoration: 'none', textTransform: 'none' }}>About</Button>
+          <Button color="inherit" href="#experience" sx={{ color: 'black', textDecoration: 'none', textTransform: 'none' }}>Experience</Button>
+          <Button color="inherit" href="#projects" sx={{ color: 'black', textDecoration: 'none', textTransform: 'none' }}>Projects</Button>
           <StyledIconButton href="https://twitter.com/jaretterer" target="_blank">
             <FontAwesomeIcon icon={faTwitter} />
           </StyledIconButton>
@@ -35,7 +83,17 @@ const NavBar = () => {
             <FontAwesomeIcon icon={faLinkedin} />
           </StyledIconButton>
         </div>
+        }
       </Toolbar>
+      <Drawer
+        variant="temporary"
+        anchor="right"
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{ keepMounted: true, marginRight: '5%' }}
+      >
+        {drawer}
+      </Drawer>
     </AppBar>
   );
 };
